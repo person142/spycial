ec1dd5d70b4a36e6d930cd9250bc85645e223110
Josh Wilson <person142@users.noreply.github.com>
2019-03-25 22:46:06 -0700

    Add more configuration for uploading to PyPI

--- setup.py
+++ setup.py
@@ -3,7 +3,10 @@ from distutils.core import setup
 
 setup(
     name='spycial',
-    version='0.0.1',
+    description='Special functions written in Python and accelerated by Numba',
+    version='0.1.1',
+    author='Josh Wilson',
+    url='https://github.com/person142/spycial',
     packages=['spycial'],
     install_requires=['numba', 'numpy'],
 )
